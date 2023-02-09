e9c1f82ee5806dbdc92f5600d115cb3446524cb9
Michael Knyazev <mikebradius@gmail.com>
2023-02-09 15:01:21 +0400

    Add primitive to types

--- types.go
+++ types.go
@@ -1,5 +1,8 @@
 package lungo
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Refs = map[string]string
 type Filter = map[string]interface{}
 type Fields = []string
+type Primitive = primitive.ObjectID
