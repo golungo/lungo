c8a514496977f701af916f100cc3e50f17df7121
Michael Knyazev <mikebradius@gmail.com>
2023-02-09 15:05:15 +0400

    I messed up with types name, haha

--- types.go
+++ types.go
@@ -5,4 +5,4 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Refs = map[string]string
 type Filter = map[string]interface{}
 type Fields = []string
-type Primitive = primitive.ObjectID
+type ObjectID = primitive.ObjectID
